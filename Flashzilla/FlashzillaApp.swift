586061aa79e4ab4310616fe278e26540a049e898
Talita Groppo <talitagroppo@icloud.com>
2021-03-29 20:36:47 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Talita Groppo on 27/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
